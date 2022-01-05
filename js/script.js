8db19babf54c7ca4d606697fc0df009120c15b62
Viet Anh <vietanh020600@gmail.com>
2022-01-05 14:08:35 +0700

    Add file

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
+
+searchBtn.onclick = () =>{
+   searchBtn.classList.toggle('fa-times');
+   searchForm.classList.toggle('active');
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
+
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+   searchBtn.classList.remove('fa-times');
+   searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   searchBtn.classList.remove('fa-times');
+   searchForm.classList.remove('active');
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
+
+$(document).ready(function () {
+   $(window).scroll(function () {
+     if ($(this).scrollTop()) {
+       $("#backtop").fadeIn();
+     } else {
+       $("#backtop").fadeOut();
+     }
+   });
+ 
+   $("#backtop").click(function () {
+     $("html,body").animate({ scrollTop: 0 }, 500);
+   });
+ });
